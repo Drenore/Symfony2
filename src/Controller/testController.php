3860fbd5fcc8fa2bba1efbc07ea650d66491ddea
Drenore <benjamin.cristinelli1@gmail.com>
2022-02-03 11:17:09 +0100

    Fin du premier cours sur les routings

--- src/Controller/testController.php
+++ src/Controller/testController.php
@@ -2,9 +2,9 @@
 
     namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
 
 class testController
     {
